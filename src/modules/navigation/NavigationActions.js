b9ff2a2e764fe532722c972658d24098fae8d0f0
tkjone <thomasmattacchione@gmail.com>
2016-06-17 16:23:15 -0400

    Refactored The Nav Actions
    - removed the payload property.  Too verbose

--- src/modules/navigation/NavigationActions.js
+++ src/modules/navigation/NavigationActions.js
@@ -13,15 +13,11 @@ import * as ACTIONS from '../../constants/ActionTypes';
 
 export const jumptToTab = (index) => (
     type: ACTIONS.JUMP_TO_TAB,
-    payload: {
-        index: index,
-    }
+    index: index,
 )
 
 export const addToTabStack = (index) => (
     type: ACTIONS.ADD_TO_TAB_STACK,
-    payload: {
-        index: index,
-    }
+    index: index,
 )
 
